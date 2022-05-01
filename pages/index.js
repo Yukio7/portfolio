dfc867d0d1faf59b1a5d2813b31fb9c13c6c3cd6
Yukio7 <arthur.manhaudier@gmail.com>
2022-05-01 17:53:03 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -76,23 +76,9 @@ const Home = () => (
           Bio
         </Heading>
         <BioSection>
-          <BioYear>1984</BioYear>
-          Born in Osaka (大阪), Japan.
+       
         </BioSection>
-        <BioSection>
-          <BioYear>2010</BioYear>
-          Completed the Master&apos;s Program in the Graduate School of
-          Information Science at Nara Institute of Science and Technology
-          (奈良先端科学技術大学院大学情報科学研究科修士課程)
-        </BioSection>
-        <BioSection>
-          <BioYear>2010</BioYear>
-          Worked at Yahoo! Japan (ヤフー株式会社入社)
-        </BioSection>
-        <BioSection>
-          <BioYear>2012 to present</BioYear>
-          Works as a freelance
-        </BioSection>
+        
       </Section>
 
         <Box align="center" my={4}>
