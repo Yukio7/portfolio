c76df08a83245cf663cf777214cf1788dcb40175
Arthur <73617280+Yukio7@users.noreply.github.com>
2022-05-03 09:30:08 +0200

    Update bts_sio.js

--- pages/bts_sio.js
+++ pages/bts_sio.js
@@ -11,7 +11,9 @@ const Works = () => (
         LE BTS SIO
       </Heading>
        <p> 
-        Le bts sio est 
+        Le Brevet de Technicien Supérieur Services Informatiques aux Organisations (BTS SIO) est un diplôme reconnu par l’état de niveau Bac+2. Il associe aux enseignements théoriques et professionnels, deux stages en entreprises (10 à 11 semaines réparties sur les deux années d’études).
+
+En plus des matières en lien avec l’ingénierie informatique, la formation propose également des enseignements de culture générale, d’anglais, de mathématiques appliquées à l’informatique, ainsi que sur les outils d’analyse économique, managériale et juridique des services informatiques.
        </p>
 
       </Section>
