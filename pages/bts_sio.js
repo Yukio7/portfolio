9bcf1d3b2428f51589c485b7c497ffaa4b19b084
Yukio7 <arthur.manhaudier@gmail.com>
2022-05-06 23:56:34 +0200

    Commit message

--- pages/bts_sio.js
+++ pages/bts_sio.js
@@ -4,7 +4,7 @@ import Layout from '../components/layouts/article'
 import Paragraph from '../components/paragraph'
 import Section from '../components/section'
 import { Stack, HStack, VStack,Flex, Spacer } from '@chakra-ui/react'
-
+import { useState } from "react";
 import Head from "next/head";
 import {
   SlideFade,
@@ -15,22 +15,7 @@ import {
   Tag,
 } from "@chakra-ui/react";
 
-<<<<<<< HEAD
-import { useState } from "react";
-=======
-const Works = () => (
-  <Layout title="Le BTS SIO">
-    <Container>
-      <Section delay={0.1} >
-      <Heading as="h3" fontSize={20} mb={4}>
-        LE BTS SIO
-      </Heading>
-       <p> 
-        Le Brevet de Technicien Supérieur Services Informatiques aux Organisations (BTS SIO) est un diplôme reconnu par l’état de niveau Bac+2. Il associe aux enseignements théoriques et professionnels, deux stages en entreprises (10 à 11 semaines réparties sur les deux années d’études).
 
-En plus des matières en lien avec l’ingénierie informatique, la formation propose également des enseignements de culture générale, d’anglais, de mathématiques appliquées à l’informatique, ainsi que sur les outils d’analyse économique, managériale et juridique des services informatiques.
-       </p>
->>>>>>> c76df08a83245cf663cf777214cf1788dcb40175
 
 const About = () => {
   const [show, setShow] = useState(false);
