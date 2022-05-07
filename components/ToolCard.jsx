631cdda8ea39b3bb1c51e4a9066d0118e1ad6885
Yukio7 <arthur.manhaudier@gmail.com>
2022-05-07 12:09:05 +0200

    Commit message

--- components/ToolCard.jsx
+++ components/ToolCard.jsx
@@ -6,7 +6,7 @@ import {
   LinkBox,
   useColorModeValue,
 } from "@chakra-ui/react";
-import Paragraph from "./Paragraph";
+import { Paragraph } from "@emotion/styled";
 
 const ToolCard = ({ tool }) => {
   return (
